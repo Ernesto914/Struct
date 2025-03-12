62574afd359f4aacc6d8378f01249f9573655ea0
Ernesto914 <ernestolupicino914@gmail.com>
2025-03-12 08:35:58 +0100

    File

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+struct Classe {
+    int eta[20];
+    float voti[20];
+    float media;
+};
+
+int main(void)
+{
+    Classe MiaClasse;
+
+    do {
+        printf("Inserisci eta': ");
+        scanf("%d", &MiaClasse.eta);
+    } while ()
+
+
+}
